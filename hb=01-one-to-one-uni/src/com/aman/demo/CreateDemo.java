ca8ce9cce34bddae13a81bb9da0b8d95b02ace53
amksingh <aman.priyadarshi1706@gmail.com>
2022-12-13 21:05:47 +0530

    new code

---
+++ hb=01-one-to-one-uni/src/com/aman/demo/CreateDemo.java
@@ -0,0 +1,53 @@
+package com.aman.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ama.entity.Instructor;
+import com.ama.entity.InstructorDetails;
+
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+	
+		//create session factory
+		SessionFactory factory=new Configuration().configure()
+									.addAnnotatedClass(InstructorDetails.class)
+									.addAnnotatedClass(Instructor.class).buildSessionFactory();
+		//create the session
+		Session session=factory.getCurrentSession();
+		
+		try{
+			//create the object 
+//			InstructorDetails tempinInstructorDetails=new InstructorDetails("http://www.ama.com/youtube"
+//					,"cricket");
+//			Instructor teInstructor=new Instructor("aman","kumar","aman@gmail.com");
+			
+			InstructorDetails tempinInstructorDetails=new InstructorDetails("http://youtube.yahoo"
+					,"TableTennis");
+			Instructor teInstructor=new Instructor("sunidhi","mishra","Somi@gmail.com");
+			
+			
+			//associate the objects
+			teInstructor.setInstructorDetails(tempinInstructorDetails);
+			
+			
+			//start the transaction
+			session.beginTransaction();
+			session.save(teInstructor);
+			
+			
+			//commit the transaction
+			session.getTransaction().commit();
+			
+		}
+		finally {
+			factory.close();
+		}
+		
+		
+	}
+
+}
